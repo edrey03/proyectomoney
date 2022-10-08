cc118770bf167a95e7c780d60d2e5f24d161b118
edrey03 <edreymarlon03@gmail.com>
2022-10-08 18:41:55 -0500

    first commit

---
+++ src/components/AsideMenu.jsx
@@ -0,0 +1,50 @@
+function AsideMenu() {
+    return (
+        <aside>
+            <div className="">
+                <ul>
+                    <li>
+                        <ion-icon name="home-sharp"></ion-icon>
+                        <p>Home</p>
+                    </li>
+                    <li>
+                        <ion-icon name="home-sharp"></ion-icon>
+                        <p>rigvhdsouifvh</p>
+                    </li>
+                    <li>
+                        <ion-icon name="home-sharp"></ion-icon>
+                        <p>rigvhdsouifvh</p>
+                    </li>
+                    <li>
+                        <ion-icon name="home-sharp"></ion-icon>
+                        <p>rigvhdsouifvh</p>
+                    </li>
+                    <li>
+                        <ion-icon name="home-sharp"></ion-icon>
+                        <p>rigvhdsouifvh</p>
+                    </li>
+                </ul>
+            </div>
+            <div className="aside-footer">
+                <div className="fields-footer">
+                    <div className="">
+                        <ion-icon name="stats-chart-sharp"></ion-icon>
+                    </div>
+                    <div className="">
+                        <div className="">
+                            <ion-icon name="logo-whatsapp"></ion-icon>
+                        </div>
+                        <div className="">
+                            <ion-icon name="logo-twitter"></ion-icon>
+                        </div>
+                    </div>
+                </div>
+                <div className="fields-footer">
+                    <div className=""></div>
+                </div>
+            </div>
+        </aside>
+    );
+}
+
+export default AsideMenu;
