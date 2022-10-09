65baa9cbc3942e37446cb084a61cd62d7405e5e5
rossianllela <112584537+rossianllela@users.noreply.github.com>
2022-10-08 23:54:29 -0400

    farm

--- src/components/Farm.jsx
+++ src/components/Farm.jsx
@@ -5,15 +5,41 @@ function Farm () {
         <>
             <div className="home-container">
                 <AsideMenu />
-                <article>
                 <section class="info">
                 <h2><center>Money Farm</center></h2>
                 <h2><center>Stake Liquidity Pool (LP) tokens to earn.</center></h2>
                 <div class="contenedorfarm">  
                 </div>
             </section>
-                </article>
-            </div>
+
+            <div className="div-time">
+                        <div className=""></div>
+                        <div className=""></div>
+
+            <div className="div-time-grow"></div>
+                        <div className=""></div>
+                        <div className=""></div>
+                    </div>
+
+                    <div className="div-time">
+                        <div className=""></div>
+                        <div className=""></div>
+                        
+            <div className="div-time-grow"></div>
+                        <div className=""></div>
+                        <div className=""></div>
+                    </div>
+
+                    <div className="div-time">
+                        <div className=""></div>
+                        <div className=""></div>
+                        
+            <div className="div-time-grow"></div>
+                        <div className=""></div>
+                        <div className=""></div>
+                    </div>
+                </div>
+            
         </>
     );
 }
