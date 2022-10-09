423ef5eb9c317e2d8688c71892408af49fb53fab
rossianllela <112584537+rossianllela@users.noreply.github.com>
2022-10-09 15:06:06 -0400

    Update Farm.jsx

--- src/components/Farm.jsx
+++ src/components/Farm.jsx
@@ -5,39 +5,43 @@ function Farm () {
         <>
             <div className="home-container">
                 <AsideMenu />
+                <article>
                 <section class="info">
                 <h2><center>Money Farm</center></h2>
                 <h2><center>Stake Liquidity Pool (LP) tokens to earn.</center></h2>
                 <div class="contenedorfarm">  
                 </div>
             </section>
-
-            <div className="div-farm">
-                        <div className=""></div>
-                        <div className=""></div>
-
-            <div className="div-farm-grow"></div>
-                        <div className=""></div>
-                        <div className=""></div>
-                    </div>
-
-                    <div className="div-farm">
-                        <div className=""></div>
-                        <div className=""></div>
-                        
-            <div className="div-farm-grow"></div>
-                        <div className=""></div>
-                        <div className=""></div>
-                    </div>
-
-                    <div className="div-farm">
-                        <div className=""></div>
-                        <div className=""></div>
-                        
-            <div className="div-farm-grow"></div>
-                        <div className=""></div>
-                        <div className=""></div>
-                    </div>
+        <br>
+        </br>
+        <div className="cartas">
+                    <h3>
+                       EARN UP TO
+                    </h3>
+                    <h1>124.67% APR</h1>
+                    <h3>
+                    IN MONEY FARMS
+                    </h3>
+                </div>  
+                <div className="cartos">
+                    <h3>
+                    EARN
+                    </h3>
+                    <h1>MONEY</h1>
+                    <h3>
+                    IN POOLS
+                    </h3>
+                </div> 
+                <div className="cartas">
+                    <h3>
+                    LOTTERY WITH
+                    </h3>
+                    <h1>$4</h1>
+                    <h3>
+                    UP FOR GRABS
+                    </h3>
+                </div>  
+      </article>
                 </div>     
         </>
     );
