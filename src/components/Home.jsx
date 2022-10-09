48851d60eb24e1474446e163ceac7dc6500cebb7
rossianllela <112584537+rossianllela@users.noreply.github.com>
2022-10-08 21:41:24 -0400

    mejoras

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,16 @@ function Home () {
                 <AsideMenu />
                 <article>
                     <div className="div-time">
-                        <div className=""></div>
-                        <div className=""></div>
+                        <div className="">
+                        <button className="button2">Collect winnings</button>
+                        </div>
+                    <span class="sc-kHdrYz giTQXq">Massive burn in:</span>
+                    <div className="circulos">
+                        <div className="circulo"><svg id="svg1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="gradient1" x1="0" x2="0" y1="0" y2="1"><stop offset="0%" stop-color="#43cdfc"></stop><stop offset="20%" stop-color="#40cbfd"></stop><stop offset="100%" stop-color="#2b80f5"></stop></linearGradient></defs><g fill="none" stroke="none"><circle cx="50" cy="50" r="47" stroke-width="5" stroke="url(#gradient1)"></circle><circle cx="50" cy="50" r="47" transform="rotate(90 50 50)" stroke-dashoffset="0, 314" stroke-dasharray=" 0, 314" stroke="#15181B" stroke-width="7"></circle></g></svg></div>
+                        <div className="circulo"><svg id="svg1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="gradient1" x1="0" x2="0" y1="0" y2="1"><stop offset="0%" stop-color="#43cdfc"></stop><stop offset="20%" stop-color="#40cbfd"></stop><stop offset="100%" stop-color="#2b80f5"></stop></linearGradient></defs><g fill="none" stroke="none"><circle cx="50" cy="50" r="47" stroke-width="5" stroke="url(#gradient1)"></circle><circle cx="50" cy="50" r="47" transform="rotate(90 50 50)" stroke-dashoffset="0, 314" stroke-dasharray=" 0, 314" stroke="#15181B" stroke-width="7"></circle></g></svg></div>
+                        <div className="circulo"><svg id="svg1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="gradient1" x1="0" x2="0" y1="0" y2="1"><stop offset="0%" stop-color="#43cdfc"></stop><stop offset="20%" stop-color="#40cbfd"></stop><stop offset="100%" stop-color="#2b80f5"></stop></linearGradient></defs><g fill="none" stroke="none"><circle cx="50" cy="50" r="47" stroke-width="5" stroke="url(#gradient1)"></circle><circle cx="50" cy="50" r="47" transform="rotate(90 50 50)" stroke-dashoffset="0, 314" stroke-dasharray=" 0, 314" stroke="#15181B" stroke-width="7"></circle></g></svg></div>
+                        </div>
+
                         <div className=""></div>
                         <div className=""></div>
                         <div className=""></div>
@@ -20,10 +28,10 @@ function Home () {
                         <div className=""></div>
                         <div className=""></div>
                     </div>
-                    <div className="">
+                    <div className="div-right">
                         <h2>KNSODFGIHNSEPIV</h2>
-                    </div>
-                    <div className="">
+                        </div>
+                    <div className="div-left">
                         <p>ksefhodisrhfgoidsrhfg</p>
                     </div>
                     <div className="cards-dapps-container">
