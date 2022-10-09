e69690727860ac0783f62dd9aa4a5fe2940125ed
rossianllela <112584537+rossianllela@users.noreply.github.com>
2022-10-09 00:21:15 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
-import Farm from './components/Farm';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
-      <Farm />
     </div>
   );
 }
