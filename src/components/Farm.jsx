a8691f291819ea18bc1ee409f7733f74b15815d2
rossianllela <112584537+rossianllela@users.noreply.github.com>
2022-10-08 23:20:49 -0400

    farm
    adelanto

--- src/components/Farm.jsx
+++ src/components/Farm.jsx
@@ -6,7 +6,12 @@ function Farm () {
             <div className="home-container">
                 <AsideMenu />
                 <article>
-                    
+                <section class="info">
+                <h2><center>Money Farm</center></h2>
+                <h2><center>Stake Liquidity Pool (LP) tokens to earn.</center></h2>
+                <div class="contenedorfarm">  
+                </div>
+            </section>
                 </article>
             </div>
         </>
