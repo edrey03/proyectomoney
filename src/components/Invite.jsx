b02e0f01b5286914207d3f14e0a3970a2dd8b652
rossianllela <112584537+rossianllela@users.noreply.github.com>
2022-10-09 22:36:33 -0400

    Invitados

--- src/components/Invite.jsx
+++ src/components/Invite.jsx
@@ -3,20 +3,23 @@ import AsideMenu from './AsideMenu';
 function Invite () {
     return (
         <>
-            <div className="home-container">
+              <div className="home-container">
                 <AsideMenu />
                 <article>
-                    <br>
-                    </br>
-                <h2><center>Refer And Earn</center></h2>
-                <div className="div-refer">
-                        <div className=""><center><h4>Share the referral link below to invite your friends and earn 2% of your friends earnings FOREVER!</h4></center></div>
-                        <button className="unlock-button">UNLOCK WALLET</button>
-                        <div className=""><center><h5>Your will automatically get $ONI in your wallet as reward whenever the referred user Harvests/Withdraws from our ONI Farms and Fuel Pools.</h5></center></div>
-                        </div>
-                </article>
-            </div>
-
+       <h2><center>Refer And Earn</center></h2>
+       <div className="cartites-invite">
+       <div className="cartites">
+                    <h4>
+                    Share the referral link below to invite your friends and earn 2% of your friends earnings FOREVER!  
+                    </h4>
+                    <button className="button-invite">Unlock Wallet</button>  
+                    <h4>
+                    Your will automatically get $Money in your wallet as reward whenever the referred user Harvests/Withdraws from our ONI Farms and Fuel Pools.
+                    </h4>
+                </div> 
+                </div>  
+        </article>
+                </div>       
         </>
     );
 }
