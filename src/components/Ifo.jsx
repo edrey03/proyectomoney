23e99feaba33452a7625ff529c1af44a5e8acb8f
rossianllela <112584537+rossianllela@users.noreply.github.com>
2022-10-09 23:13:24 -0400

    .
    .

--- src/components/Ifo.jsx
+++ src/components/Ifo.jsx
@@ -7,51 +7,16 @@ function Ifo () {
                 <AsideMenu />
                 <article>
                 <section class="info">
-                <h2><center>The ONI Lottery</center></h2>
-                <h4><center>Buy tickets with Money Win if 2, 3, or 4 of your ticket numbers match!</center></h4>
+                <h2><center>IFO: Initial</center></h2>
+                <h2><center>Farm</center></h2>
+                <h2><center>Offerings</center></h2>
+                <h3><center>Buy new tokens with a brand new token sale model.</center></h3>
                 <div class="contenedorfarm">  
                 </div>
             </section>
         <br>
         </br>
-        <div className="cartes-farm">
-        <div className="cartes">
-                    <h3>
-                       EARN UP TO
-                    </h3>
-                    <h1>124.67% APR</h1>
-                    <h3>
-                    IN MONEY FARMS
-                    </h3>
-                </div>  
-                <div className="cartes">
-                    <h3>
-                    EARN
-                    </h3>
-                    <h1>MONEY</h1>
-                    <h3>
-                    IN POOLS
-                    </h3>
-                </div> 
-                </div> 
-       <br>
-       </br>
-       <h2><center>HOW IT WORKS</center></h2>
-       <h4><center>Spend ONI to buy tickets, contributing to the lottery pot. Win prizes if 2, 3, or 4 of your ticket numbers match the winning numbers and their exact order!</center></h4>
-       <br>
-       </br>
-       <h4><center>READ MORE</center></h4>
-       <br>
-       </br>
-       <div className="cartitas">
-                    <h3>
-                       EARN UP TO
-                    </h3>
-                    <h1>124.67% APR</h1>
-                    <h3>
-                    IN MONEY FARMS
-                    </h3>
-                </div>  
+  
         </article>
                 </div>       
         </>
